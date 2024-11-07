93ee3e54b98e8ee5a165b8d6d35a7a0d23d4e982
Hariom-Ingle <hariomingle2003@gmail.com>
2024-11-08 01:25:19 +0530

    ""

--- backend/index.js
+++ backend/index.js
@@ -45,9 +45,9 @@ app.use("/api/v1/application", applicationRoute);
 app.use("/api/v1/admin", adminRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend","dist", "index.html"));
   });
 }
 
