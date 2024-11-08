e0c72d1698958748bfd3d280ca1d23b3cd682ac4
Hariom-Ingle <hariomingle2003@gmail.com>
2024-11-08 21:27:11 +0530

    "backend"

--- backend/index.js
+++ backend/index.js
@@ -54,13 +54,13 @@ app.use("/api/v1/admin", adminRoute);
 //   });
 // }
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
+// 	app.get("*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
 
 // Start Server and Connect to Database
 app.listen(PORT, async () => {
