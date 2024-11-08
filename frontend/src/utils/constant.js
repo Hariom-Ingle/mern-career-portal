e0ee776f60d179a995f6bc7cd914a42e0d2698e4
Hariom-Ingle <hariomingle2003@gmail.com>
2024-11-08 21:33:03 +0530

    "final commit"

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
-export const ADMIN_API_END_POINT="http://localhost:8000/api/v1/admin";
+export const USER_API_END_POINT="https://job-portal-backend-3s57.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://job-portal-backend-3s57.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT="https://job-portal-backend-3s57.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT="https://job-portal-backend-3s57.onrender.com/api/v1/company";
+export const ADMIN_API_END_POINT="https://job-portal-backend-3s57.onrender.com/api/v1/admin";
